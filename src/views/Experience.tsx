221d293ea960812f535e55c5cc389292966d63f3
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-06-25 11:31:31 +0200

    add animation libary, add view fro mobile for experience section

--- src/views/Experience.tsx
+++ src/views/Experience.tsx
@@ -3,11 +3,10 @@ import { historyHistory } from "../json/experienceHistory.json";
 import { experience } from "../json/experience.json";
 import HistorySideList from "../components/HistorySideList";
 import ExperienceCard from "../components/ExperienceCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Experience = () => {
   const [activeElement, setActiveElement] = useState<number>(0);
-
   function handleActive(index: number) {
     setActiveElement(index);
   }
