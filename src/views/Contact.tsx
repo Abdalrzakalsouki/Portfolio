b45c831ece526191c3661e83060176fa4ffb6571
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-06-26 19:04:54 +0200

    add form valdation

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,62 +1,88 @@
 import "../css/contact.css";
-import { useForm, ValidationError } from "@formspree/react";
+import { useForm } from "@formspree/react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 const Contact = () => {
   const key = import.meta.env.VITE_FORM_ENDPOINT;
-  const [state, handleSubmit] = useForm(`${key}`);
-  if (state.succeeded) {
-    return (
-      <p className="sucess-submition">
-        Recivied, We will get back to you soon!
-      </p>
-    );
-  }
-
+  const [state, handleBackedSubmition] = useForm(`${key}`);
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .nullable()
+      .matches(
+        /^[a-zA-Z\s]*$/,
+        "Invalid name. Only letters and spaces are allowed"
+      ),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    message: Yup.string().required("Message is required"),
+  });
   return (
     <div className="contact-co" id="contact">
       <h2>Get in touch</h2>
-      <form className="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name"> Name </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="ex: Olivia Anderson"
-          />
-        </div>
-        <div>
-          <label htmlFor="email">
-            Email address <span>*</span>
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="ex: olivia92@gmail.com"
-            required
-          />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-        </div>
-        <div>
-          <label htmlFor="message">
-            Message <span>*</span>
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            placeholder="ex: Let is connect!"
-            required
-          ></textarea>
-          <ValidationError
-            prefix="Message"
-            field="message"
-            errors={state.errors}
-          />
-        </div>
-        <button type="submit" disabled={state.submitting}>
-          Send
-        </button>
-      </form>
+      <Formik
+        initialValues={{ name: "", email: "", message: "" }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm, setStatus }) => {
+          handleBackedSubmition(values);
+          resetForm();
+          setStatus("Recivied, We will get back to you soon!");
+          setSubmitting(false);
+          setTimeout(() => {
+            setStatus("");
+          }, 3000);
+        }}
+      >
+        {({ status }) => (
+          <Form className="contact-form">
+            <div>
+              <label htmlFor="name"> Name </label>
+              <Field
+                type="text"
+                name="name"
+                placeholder="ex: Olivia Anderson"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="erro-message"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">
+                Email address <span>*</span>
+              </label>
+              <Field
+                type="email"
+                name="email"
+                placeholder="ex: olivia92@gmail.com"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="erro-message"
+              />
+            </div>
+            <div>
+              <label htmlFor="message">
+                Message <span>*</span>
+              </label>
+              <Field
+                as="textarea"
+                name="message"
+                placeholder="ex: Let is connect!"
+              />
+              <ErrorMessage
+                name="message"
+                component="div"
+                className="erro-message"
+              />
+            </div>
+            <button type="submit">Submit</button>
+            {status && <p className="sucess-submition">{status}</p>}
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
