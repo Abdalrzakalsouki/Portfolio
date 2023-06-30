6227352232ba6230fc95b49dc53d96afcfcd6c23
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-06-30 18:06:33 +0200

    add links to mobile list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -29,11 +29,21 @@ function List() {
         onClick={() => setShowList(!showList)}
       />
       <ul aria-hidden="true" className={showList ? "show" : "hide"}>
-        <li>Home</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Experience</li>
-        <li>Contact</li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#education">Education</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
     </div>
   );
